299ac4916f5a010df685ed7410149b05ff034a98
Jeffrey Armstrong <jeff@approximatrix.com>
2026-05-20 13:08:08 -0400

    Added fixes for vertex format missing in cone and torus as well.

--- procedural3d/cone.py
+++ procedural3d/cone.py
@@ -768,6 +768,7 @@ class ConeMaker(ModelMaker):
                     vertex_format = GeomVertexFormat.get_v3n3c4t2()
                 else:
                     vertex_format = GeomVertexFormat.get_v3n3c4()
+                vertex_data.set_format(vertex_format)
                 vertex_data = vertex_data.set_color(self._vertex_color)
                 geom.set_vertex_data(vertex_data)
 
